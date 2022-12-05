2704a54324cc17597410fe0276ce5c600ae8bb15
Kanishk Kumar <kanishka_408@yahoo.co.in>
2022-12-05 17:05:22 +0000

    Initial Commit

---
+++ GraphQL-StarWars/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GraphQL-StarWars
+//
+//  Created by Kanishk Kumar on 05/12/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GraphQL-StarWars/GraphQL_StarWarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphQL_StarWarsApp.swift
+//  GraphQL-StarWars
+//
+//  Created by Kanishk Kumar on 05/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct GraphQL_StarWarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
